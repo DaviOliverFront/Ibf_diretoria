6c635540497d2aac6bde8b19d8b5211fcab5957d
Davi Oliveira Sousa <davioliveirs6@gmail.com>
2025-09-01 19:33:53 -0300

    DropDown
    Criando o DropDown com css e js

--- aplicacao/src/components/Form/Form.js
+++ aplicacao/src/components/Form/Form.js
@@ -1,7 +1,17 @@
+import DropDown from '../DropDown/DropDown';
 import TextField from '../TextField/TextField'
 import './Form.css'
 
 const Form = () => {
+
+    const times = [
+        '',
+        'Presidência',
+        'Diáconos',
+        'Secretários',
+        'Tesoureiros',
+    ];
+
     return (
         <section className="formu">
             <form>
@@ -9,6 +19,7 @@ const Form = () => {
                 <TextField label="Nome:" placeholder="Digite seu nome"/>
                 <TextField label="Cargo:" placeholder="Informe seu cargo"/>
                 <TextField label="Imagem:" placeholder="Adicione uma foto"/>
+                <DropDown label="Time:" itens={times}/>
             </form>
         </section>
     )
